b5f95c355260c880adee8fefd17b336df0841ad6
lauren-briggs <laurenbriggs41@gmail.com>
2021-05-21 09:54:36 +1000

    return arrow to default on resize, added animation to work p and added gradient at top and bottom of rightCont

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,49 @@
+// THE /\RCTIC /\/\IRAGE
 $(document).ready(function () {
   //js will only run once HTML doc has loaded to speed up response time
 
+  // ----------------------------------------------------
   // ELEMENTS
+  // ----------------------------------------------------
   const menuEl = $(".menu");
   const rightCont = $("#rightcont");
   const leftCont = $(".leftcont");
   const workEl = $(".work")
   const aboutEl = $(".about")
   const contactEl = $(".contact")
+  const workGridCont = $(".grid-container-workmenu");
+  const workGridItem = $(".grid-item-workmenu");
 
+  // ----------------------------------------------------
   // BUTTONS
+  // ----------------------------------------------------
   const navBtn = $("#navBtn")
   const workNav = $("#workNav");
   const aboutNav = $("#aboutNav");
   const contactNav = $("#contactNav")
   const amLogo = $("#homelogo")
 
+  // ----------------------------------------------------
   // BOOLEANS TO DEFINE WHETHER OR NOT SECTIONS ARE OPEN
-  var isRightContOpen = false;
-  var isLeftContOpen = false;
-  var isWorkElOpen = false;
-  var isAboutElOpen = false;
-  var isContactElOpen = false;
-  var isDescrOpen = false;
+  // ----------------------------------------------------
+  let isRightContOpen = false;
+  let isLeftContOpen = false;
+  let isWorkElOpen = false;
+  let isAboutElOpen = false;
+  let isContactElOpen = false;
+  let isDescrOpen = false;
 
+  // ----------------------------------------------------
   // SHOW INITIAL MENU ON HOVER
+  // ----------------------------------------------------
   function showMenu() {
     menuEl.addClass("showme");
     navBtn.addClass("showme");
   }
 
+  // ----------------------------------------------------
   // HIDE MENU/CLOSE ALL
+  // ----------------------------------------------------
   function closeAll() {
     // checking if right container is open and closing if true
     if (isRightContOpen === true) {
@@ -45,13 +58,13 @@ $(document).ready(function () {
 
     // checking if left container is open and closing if true
     if (isLeftContOpen === true) {
-      $(".leftcont").children().each(function () {
+      leftCont.children().each(function () {
         $(this).css("display", "none")
       });
     }
     // Checking if other descriptions are open and closing if true
     if (isDescrOpen === true) {
-      $(".grid-item-workmenu").children("div").each(function () {
+      workGridItem.children("div").each(function () {
         $(this).children("p").each(function () {
           $(this).css("display", "none");
         })
@@ -74,7 +87,9 @@ $(document).ready(function () {
     contactNav.removeClass("underline");
   }
 
+  // ----------------------------------------------------
   // HANDLE MENU FUNCTION FOR ALL NAV ITEMS - changing text to white etc.
+  // ----------------------------------------------------
   function handleMenu() {
     menuEl.removeClass("showme");
     rightCont.addClass("showme");
@@ -88,15 +103,24 @@ $(document).ready(function () {
     isAboutElOpen = false;
     isContactElOpen = false;
   }
+  // ----------------------------------------------------
   // WORK WORK WORK
   // WHEN WORK IS OPEN ADD UNDERLINE AND SHOW DIV
+  // ----------------------------------------------------
   function workActive() {
     workNav.addClass("underline");
     workEl.addClass("showme");
   }
 
+  // ----------------------------------------------------
   // SHOW WORK FUNCTIONALITY AND CONDITIONS
+  // ----------------------------------------------------
   function showWork() {
+    // window.scroll({
+    //   top: 0,
+    //   left: 0,
+    //   behavior: 'smooth'
+    // });
     if (isRightContOpen === false) {
       handleMenu();
       workActive();
@@ -115,6 +139,7 @@ $(document).ready(function () {
       // handle basic menu function and show Work
       handleMenu();
       workActive();
+      resizeWork();
 
       // redefine values as true
       isRightContOpen = true;
@@ -125,21 +150,20 @@ $(document).ready(function () {
       isRightContOpen = false;
       isWorkElOpen = false;
     }
-    window.scroll({
-      top: 0,
-      left: 0,
-      behavior: 'smooth'
-    });
   }
 
+  // ----------------------------------------------------
   // ABOUT ABOUT ABOUT
   // WHEN ABOUT IS OPEN ADD UNDERLINE AND SHOW DIV
+  // ----------------------------------------------------
   function aboutActive() {
     aboutNav.addClass("underline");
     aboutEl.addClass("showme");
   }
 
+  // ----------------------------------------------------
   // SHOW ABOUT FUNCTIONALITY AND CONDITIONS
+  // ----------------------------------------------------
   function showAbout() {
     if (isRightContOpen === false) {
       handleMenu();
@@ -158,13 +182,13 @@ $(document).ready(function () {
 
       // Checking if project photos are open and closing if true
       if (isLeftContOpen === true) {
-        $(".leftcont").children().each(function () {
+        leftCont.children().each(function () {
           $(this).css("display", "none")
         });
       }
       // Checking if project descriptions are open and closing if true
       if (isDescrOpen === true) {
-        $(".grid-item-workmenu").children("div").each(function () {
+        workGridItem.children("div").each(function () {
           $(this).children("p").each(function () {
             $(this).css("display", "none");
           })
@@ -192,14 +216,18 @@ $(document).ready(function () {
       behavior: 'smooth'
     });
   }
+  // ----------------------------------------------------
   // CONTACT CONTACT CONTACT
   // WHEN CONTACT IS OPEN ADD UNDERLINE AND SHOW DIV
+  // ----------------------------------------------------
   function contactActive() {
     contactNav.addClass("underline");
     contactEl.addClass("showme");
   }
 
+  // ----------------------------------------------------
   // SHOW CONTACT FUNCTIONALITY AND CONDITIONS
+  // ----------------------------------------------------
   function showContact() {
     if (isRightContOpen === false) {
       handleMenu();
@@ -218,13 +246,13 @@ $(document).ready(function () {
 
       // Checking if project photos are open and closing if true
       if (isLeftContOpen === true) {
-        $(".leftcont").children().each(function () {
+        leftCont.children().each(function () {
           $(this).css("display", "none")
         });
       }
       // Checking if project descriptions are open and closing if true
       if (isDescrOpen === true) {
-        $(".grid-item-workmenu").children("div").each(function () {
+        workGridItem.children("div").each(function () {
           $(this).children("p").each(function () {
             $(this).css("display", "none");
           })
@@ -253,7 +281,9 @@ $(document).ready(function () {
     });
   }
 
+  // ----------------------------------------------------
   // Changing AM logo to white version when screen is less than 1024px
+  // ----------------------------------------------------
   function swapLogo() {
     if ($(window).width() <= 1024 && isRightContOpen === true) {
       $("#replace-logo").attr("src", "assets/img/am-collateral/AM20-29_TAM Internal Marketing_Logo_Fin_AM_Rev.png");
@@ -263,33 +293,34 @@ $(document).ready(function () {
   };
 
   $(window).resize(swapLogo);
-
+  // ----------------------------------------------------
   // SHOW PROJECT IMAGES AND DESCRIPTION ON CLICK
+  // ----------------------------------------------------
   function showProject() {
     // Checking if left side/other project images are open and closing them if true
     if (isLeftContOpen === true) {
-      $(".leftcont").children().each(function () {
+      leftCont.children().each(function () {
         $(this).css("display", "none")
       });
     }
-    window.scroll({
-      top: 0,
-      left: 0,
-      behavior: 'smooth'
-    });
+    // window.scroll({
+    //   top: 0,
+    //   left: 0,
+    //   behavior: 'smooth'
+    // });
 
     // Getting the id value of the target el
-    var i = $(this).attr("id");
+    let i = $(this).attr("id");
 
     // creating a variable that holds the class name using i value and changing display to flex
-    var thisProClass = `.project${i}`;
+    let thisProClass = `.project${i}`;
 
     $(`${thisProClass}`).css({ "display": "flex", "transform": "translateX(0%)" });
 
 
     // Checking if other descriptions are open and closing if true
     if (isDescrOpen === true) {
-      $(".grid-item-workmenu").children("div").each(function () {
+      workGridItem.children("div").each(function () {
         $(this).children("p").each(function () {
           $(this).css("display", "none");
         })
@@ -297,43 +328,113 @@ $(document).ready(function () {
       isDescrOpen = false;
     }
 
+    // Showing project description and adding drop down in animation
+    let children = $(`.${i}descr`);
+    children.css({
+      "display": "block",
+      "transition": "all 0.5s ease-in",
+      "animation-name": "drop-down",
+      "animation-duration": "750ms",
+      "animation-iteration-count": "1",
+      "transform-origin": "top center",
+      "animation-fill-mode": "forwards"
+    });
+
     // TO DO: check if description/project is open and close when title is clicked
 
+    let position = $(this).position();
+    console.log(position.top);
 
-    // TO DO: if window is less than 1024, make work el half screen height
-    // if (window.width() <= 1024) {
-    //   workEl.css("height", "50vh");
-    // }
+    // SCROLLING TO DESCRIPTION
+    // workGridCont.scroll({
+    //   top: position.top,
+    //   left: position.left,
+    //   behavior: 'smooth'
+    // });
 
-    // Showing project description
-    var children = $(`.${i}descr`);
-    children.css("display", "block");
+
+    // $(this).scrollIntoView(true);
+    // $(`#${i}`).scrollIntoView({ behavior: "smooth", block: "start" });
+
+    // workGridCont.scrollTop = -50;
+    // rightCont.scrollTo({ top: `${position.top}`, left: `${position.left}` }, 800);
+
+    position = $(this).position()
+
 
     isDescrOpen = true;
     isLeftContOpen = true;
+    resizeWork();
   }
 
   // TO DO: change height of left/right divs depending on window width
-  $(window).on('load resize', function () {
+  // $(window).on('load resize', function () {
+  //   if ($(window).width() < 1024) {
+  //     $(".leftcont").find(".resize").css("min-height", "50vh");
+  //     if (isLeftContOpen === false) {
+  //       console.log(`Is left container open: ${isLeftContOpen}`)
+  //     } else {
+  //       $(".work.showme").css({ "height": "50vh", "margin-top": "50vh" });
+  //       console.log($(".work.showme"))
+  //       console.log(`Is left container open: ${isLeftContOpen}`)
+  //     }
+  //   } else {
+  //     console.log(`Is left container open: ${isLeftContOpen}`)
+  //     $(".leftcont").find(".resize").css("min-height", "100vh");
+  //     $(".work.showme").css({ "height": "100vh", "margin-top": "0vh" });
+  //   }
+  // })
+
+  // ----------------------------------------------------
+  // RESIZE WORK
+  // ----------------------------------------------------
+  function resizeWork() {
+    // if window is smaller than 1024px
     if ($(window).width() < 1024) {
-      $(".leftcont").find(".resize").css("min-height", "50vh");
-      if (isLeftContOpen === false) {
-        console.log(`Is left container open: ${isLeftContOpen}`)
+      // if window is less than 600px and the left container isn't open
+      if ($(window).width() < 600 && isLeftContOpen === false) {
+        // add padding to workmenu so it sits below AM logo
+        workGridCont.css("padding-top", "8em");
       } else {
+        workGridCont.css("padding-top", "3em");
+      }
+      // find all elements in the left container with class 'resize' and change the min-height to 50vh
+      leftCont.find(".resize").css("min-height", "50vh");
+      // find arrows and move position
+      leftCont.find(".slick-prev").css("bottom", "53%");
+      leftCont.find(".slick-next").css("bottom", "53%");
+      // if the left container is not open
+      if (isLeftContOpen === false) {
+        // changr the height of the right container/work descriptions/menu to 100vh and reset the margin to 0
+        $(".work.showme").css({ "height": "100vh", "margin-top": "0vh" });
+        // if the left container and right container are both open
+      } else if (isLeftContOpen === true && isRightContOpen === true) {
+        // reduce the height of the right cont work menu to 50vh and add a top margin 
         $(".work.showme").css({ "height": "50vh", "margin-top": "50vh" });
-        console.log($(".work.showme"))
-        console.log(`Is left container open: ${isLeftContOpen}`)
+        // add top padding to work menu
+        workGridCont.css("padding-top", "3em");
       }
     } else {
-      console.log(`Is left container open: ${isLeftContOpen}`)
-      $(".leftcont").find(".resize").css("min-height", "100vh");
+      // reset height to 100vh and remove margins
+      leftCont.find(".resize").css("min-height", "100vh");
       $(".work.showme").css({ "height": "100vh", "margin-top": "0vh" });
+      // return arrow to original positions
+      leftCont.find(".slick-prev").css("bottom", "40px");
+      leftCont.find(".slick-next").css("bottom", "40px");
     }
-  })
+  }
 
 
+  // 
+
+
+  $(window).resize(resizeWork);
+  $(window).on('load', resizeWork);
+
+
+  // ----------------------------------------------------
   // SLICK CAROUSEL
-
+  // ----------------------------------------------------
   $('.slick').slick({
     adaptiveHeight: true,
     arrows: true,
@@ -353,14 +454,16 @@ $(document).ready(function () {
     verticalSwiping: true,
   });
 
+  // ----------------------------------------------------
   // CALLING FUNCTIONS
+  // ----------------------------------------------------
   navBtn.mouseenter(showMenu);
   navBtn.click(closeAll);
   amLogo.click(closeAll);
   workNav.click(showWork);
   aboutNav.click(showAbout);
   contactNav.click(showContact);
+  workGridItem.click(showProject);
   $(".slideshow-container").click(closeAll);
-  $(".grid-item-workmenu").click(showProject);
 });
 
